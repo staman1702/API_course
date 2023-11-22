1fb0e2b8d619e1212538d49ea9717e81bd93ba85
staman1702 <>
2023-11-22 17:43:39 +0000

    Add getStatus function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-va0Lw0uTRV7aLFUXqBo4NjcZNKg
+const API_KEY = "va0Lw0uTRV7aLFUXqBo4NjcZNKg";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}

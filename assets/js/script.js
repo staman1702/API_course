35c512a7942b554f12291eb39d388f9bc9a2387d
staman1702 <>
2023-11-22 17:52:09 +0000

    Add post request

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,21 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+}
 
 async function getStatus(e) {
 
@@ -13,14 +28,14 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        displatStatus(data);
+        displayStatus(data);
     }
     else {
         throw new Error(data.error);
     }
 }
 
-function displatStatus(data) {
+function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
